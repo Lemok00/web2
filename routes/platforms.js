c20238f78d69028e93f23d642e25f2825e84d6e4
Lemok00 <41943563+Lemok00@users.noreply.github.com>
2019-05-07 19:36:33 +0800

    1.2.1 新增平台的增添

--- routes/platforms.js
+++ routes/platforms.js
@@ -1,11 +1,28 @@
 const express = require('express');
-const platModel =require('../models/platforms');
+const platModel = require('../models/platforms');
 const router = express.Router();
 
-router.get('/',function (req,res) {
-    let query = platModel.find({isUnable:false},['_id','platName','coverImg']).exec(function (err,platList) {
-        res.render('platformindex',{platList:platList})
+router.get('/', function (req, res) {
+    let query = platModel.find({isUnable: false}, ['_id', 'platName', 'coverImg']).exec(function (err, platList) {
+        res.render('platforms/平台1', {platList: platList})
     });
+});
+
+router.get('/addplatforms', function (req, res) {
+    if (req.session.isLogged !== true) {
+        res.redirect('/');
+    } else {
+        res.render('platforms/addplatforms');
+    }
+});
+
+router.post('/addplatforms', function (req, res) {
+    if (req.session.isLogged !== true) {
+        res.json({ret_code: 1, ret_msg: '登录失效'});
+    } else {
+        console.log(req.body);
+        res.json({ret_code:0,ret_msg:'增添成功'});
+    }
 });
 
 module.exports = router;
