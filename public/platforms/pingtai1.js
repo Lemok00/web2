e7f200cc38cce0a321d1a889964066f5fa2a52fc
Lemok00 <41943563+Lemok00@users.noreply.github.com>
2019-05-07 23:53:36 +0800

    1.2.2 优化平台的增添，显示平台列表

--- public/platforms/pingtai1.js
+++ public/platforms/pingtai1.js
@@ -1,10 +1,11 @@
 
 $(document).ready(function(){
-    $.getJSON("/platforms","platform_JSON_package",function(data){    //有请小马哥在这一行的xxx写上后端的url链接
+    $.getJSON("/platforms/platformlist","platform_JSON_package",function(res){    //有请小马哥在这一行的xxx写上后端的url链接
+        let data=res.data;
         for(const i in data){
             const obj = data[i];                    //这里说明一下，json包不需要说明一共有多少个了，因为可以自动遍历
 
-
+            console.log(obj);
             var div1=document.createElement("div");
             var div2=document.createElement("div");
             var div3=document.createElement("div");
@@ -19,14 +20,16 @@ $(document).ready(function(){
             div2.className="img";
             div3.className="desc";
             a1.target="_blank";
-            img1.src="loading.png";
+            img1.src="/platforms/loading.png";
             img1.style.width="300";
             img1.style.height="200";
             var right=document.getElementById("right");
+            right.appendChild(div1);
 
-            img1.src=obj[2];        //根据你发给我的*那个图片*的编号这是图片
-            img1.url=obj[3];        //这是跳转链接
-            div3.innerHTML=obj[1];  //这是平台的名字
+            img1.src=obj.coverImg;        //根据你发给我的*那个图片*的编号这是图片
+            //img1.url=obj.linkedUrl;        //这是跳转链接
+            div1.setAttribute('onclick',"window.location.href='"+obj.linkedUrl+"'");
+            div3.innerHTML=obj.platName;  //这是平台的名字
 
         }
     });
--- routes/platforms.js
+++ routes/platforms.js
@@ -3,8 +3,14 @@ const platModel = require('../models/platforms');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    let query = platModel.find({isUnable: false}, ['_id', 'platName', 'coverImg']).exec(function (err, platList) {
-        res.render('platforms/平台1', {platList: platList})
+    res.render('platforms/平台1')
+});
+
+router.get('/platformlist', function (req, res) {
+    let query = platModel.find({isUnable: false}, ['_id', 'platName', 'coverImg','linkedUrl']);
+    query.exec(function (err, platList) {
+        console.log(platList);
+        res.json({data:platList});
     });
 });
 
@@ -21,7 +27,22 @@ router.post('/addplatforms', function (req, res) {
         res.json({ret_code: 1, ret_msg: '登录失效'});
     } else {
         console.log(req.body);
-        res.json({ret_code:0,ret_msg:'增添成功'});
+        let newplatform = new platModel({
+            platName: req.body.tittle,
+            coverImg: req.body.img,
+            linkedUrl: req.body.link,
+            create_date: Date.now(),
+            create_user: req.session.userName,
+            last_modified_user: req.session.userName
+        });
+        newplatform.save(function (err) {
+            if (err) {
+                console.log(err);
+                res.json({ret_code: 1, ret_msg: '平台增添失败'});
+            } else {
+                res.json({ret_code: 0, ret_msg: '平台增添成功'});
+            }
+        });
     }
 });
 
